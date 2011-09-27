451980017438b6a92e1f949d8c71027e7dadac10
Konstantin Burov <aectann@gmail.com>
2011-09-27 09:56:09 +0000

    Initial import.
    git-svn-id: https://socratica-android.googlecode.com/svn/trunk@15 8f60c059-9c78-cfce-1425-71a8f3889977

---
+++ android_typeface/src/com/socratica/mobile/TypefaceRadioButton.java
@@ -0,0 +1,22 @@
+package com.socratica.mobile;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RadioButton;
+
+import com.socratica.mobile.TypefaceView.TypefaceInitializer;
+
+/**
+ * Regular {@link RadioButton} that just allows usage of 'customTypeface' attribute to specify
+ * font for text rendering. See {@link TypefaceInitializer} for details.
+ *
+ * @author Konstantin Burov (aectann@gmail.com)
+ */
+public class TypefaceRadioButton extends RadioButton implements TypefaceView {
+
+  public TypefaceRadioButton(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    TypefaceInitializer.init(this, context, attrs);
+  }
+
+}
