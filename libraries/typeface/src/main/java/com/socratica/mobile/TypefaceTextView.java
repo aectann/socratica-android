017c6abf5b40b9da2d10cd8bf54827f5fcf1ea3e
Konstantin Burov <aectann@gmail.com>
2014-04-30 12:18:25 +0000

    Converted to gradle build system.
    git-svn-id: https://socratica-android.googlecode.com/svn/trunk@63 8f60c059-9c78-cfce-1425-71a8f3889977

---
+++ libraries/typeface/src/main/java/com/socratica/mobile/TypefaceTextView.java
@@ -0,0 +1,24 @@
+package com.socratica.mobile;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Regular {@link android.widget.TextView} that just allows usage of 'customTypeface' attribute to specify
+ * font for text rendering. See {@link com.socratica.mobile.TypefaceView.TypefaceInitializer} for details.
+ *
+ * @author Konstantin Burov (aectann@gmail.com)
+ */
+public class TypefaceTextView extends TextView implements TypefaceView {
+
+  public TypefaceTextView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    TypefaceInitializer.init(this, context, attrs);
+  }
+  
+  @Override
+  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+  }
+}
---
+++ libraries/typeface/src/main/java/com/socratica/mobile/TypefaceView.java
@@ -0,0 +1,59 @@
+package com.socratica.mobile;
+
+import com.socratica.mobile.typeface.R;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+/**
+ * Represents any view that supports text rendering with custom {@link android.graphics.Typeface}.
+ *
+ * @author Konstantin Burov (aectann@gmail.com)
+ */
+public interface TypefaceView {
+
+  /**
+   * Utility class to set custom font to a {@link com.socratica.mobile.TypefaceView}.
+   *
+   * @author Konstantin Burov (aectann@gmail.com)
+   */
+  static class TypefaceInitializer {
+    /**
+     * The method should be called by {@link com.socratica.mobile.TypefaceView} implementer constructor. It looks for a
+     * 'customTypeface' attribute on view's xml declaration. If the attribute not found on view's
+     * xml declaration the value defaults are extracted from style specified by 'typefaceViewStyle'
+     * attribute on current theme. If the theme doesn't specify the 'typefaceViewStyle' attribute,
+     * the value extracted from 'TypefaceView' style.
+     *
+     * Value of the 'customTypeface' attribute must correspond to filename of a font in '/assets'
+     * folder.
+     *
+     * For example if you have font at '/assets/Times.ttf' the customTypeface value would be
+     * 'Times.ttf'.
+     *
+     * @param view - a view that wants to use custom font for rendering.
+     * @param context - views context
+     * @param attrs - attributes applied to the view.
+     */
+    static void init(TypefaceView view, Context context, AttributeSet attrs) {
+      if (!view.isInEditMode()) {
+        TypedArray a =
+            context.obtainStyledAttributes(attrs, R.styleable.TypeFaceView,
+                R.attr.typefaceViewStyle, R.style.TypefaceView);
+        String typeFaceName = a.getString(R.styleable.TypeFaceView_customTypeface);
+        a.recycle();
+        if (typeFaceName != null) {
+          Typeface typeFace =
+              TypefaceCache.getInstance().getTypeFace(typeFaceName, context.getResources());
+          if (typeFace != null) view.setTypeface(typeFace);
+        }
+      }
+    }
+  }
+
+  void setTypeface(Typeface typeface);
+
+  boolean isInEditMode();
+}
