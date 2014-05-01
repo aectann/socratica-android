bb57e2e005457308ab9a8cd12055176e42e73d97
Konstantin Burov <aectann@gmail.com>
2014-05-01 08:58:32 +0000

    Removed settings from big image activity.
    git-svn-id: https://socratica-android.googlecode.com/svn/trunk@73 8f60c059-9c78-cfce-1425-71a8f3889977

--- example/src/main/java/com/socratica/mobile/example/BigImageActivity.java
+++ example/src/main/java/com/socratica/mobile/example/BigImageActivity.java
@@ -1,9 +1,7 @@
 package com.socratica.mobile.example;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
 
 public class BigImageActivity extends ActionBarActivity {
@@ -14,23 +12,4 @@ public class BigImageActivity extends ActionBarActivity {
         setContentView(R.layout.activity_big_image);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.big_image, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
