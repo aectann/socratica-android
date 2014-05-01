9c9ae50ee3552be914ad9f600713ff630f915f4f
aectann <aectann@8f60c059-9c78-cfce-1425-71a8f3889977>
2014-05-01 09:52:07 +0000

    Minor.
    git-svn-id: https://socratica-android.googlecode.com/svn/trunk@75 8f60c059-9c78-cfce-1425-71a8f3889977

--- example/src/main/java/com/socratica/mobile/example/data/ExamplesContent.java
+++ example/src/main/java/com/socratica/mobile/example/data/ExamplesContent.java
@@ -3,11 +3,10 @@ package com.socratica.mobile.example.data;
 import android.app.Activity;
 
 import com.socratica.mobile.example.BigImageActivity;
+import com.socratica.mobile.example.ImageMapActivity;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ExamplesContent {
 
@@ -15,15 +14,13 @@ public class ExamplesContent {
 
     static {
         addItem(new ExampleItem(BigImageActivity.class, "Big Image"));
+        addItem(new ExampleItem(ImageMapActivity.class, "Image Map"));
     }
 
     private static void addItem(ExampleItem item) {
         ITEMS.add(item);
     }
 
-    /**
-     * A dummy item representing a piece of content.
-     */
     public static class ExampleItem {
         public Class<? extends Activity>  id;
         public String content;
